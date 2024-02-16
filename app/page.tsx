1c1b67cf3549548ebc3249cd9f2c186d6fac6822
yashsinghcodes <yash9vardhan@gmail.com>
2024-02-16 14:30:31 +0000

    logo fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import { getEmbedding } from "@/lib/api";
 import Image from "next/image";
 import Link from "next/link";
 import InputBox from "@/components/input";
-import logo from "@/public/logo.png"
-
 
 export default async function Home() {
 
@@ -12,7 +10,7 @@ export default async function Home() {
     return (
         <div className="h-full w-screen">
             <div className="h-0">
-                <Image src={logo} width={210} height={0} alt="hyperbeam" />
+                <Image src="https://watch.hyperbeam.com/@/img/wordmark.429d6b13.svg" width={229.17} height={48} alt="hyperbeam" className="p-6" />
             </div>
             <div className="flex flex-col justify-center items-center h-screen">
                 <h1 className="text-center text-6xl font-bold bg-clip-text">
