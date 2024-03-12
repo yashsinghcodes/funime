a6eda5558aa11f8411a820d8a8f94d51998054f2
yashsinghcodes <yash9vardhan@gmail.com>
2024-03-12 17:38:14 +0000

    export field fix

--- app/api/del/[id]/route.ts
+++ app/api/del/[id]/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import MakeQuery from "@/lib/db";
 
 
-export default async function GET(request: Request, { params }: { params: { id: string } }) {
+export async function GET(request: Request, { params }: { params: { id: string } }) {
     MakeQuery({
         query: "DELET FROM VM WHERE `id` = ?;",
         values: [params.id]
