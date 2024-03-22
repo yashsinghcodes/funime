7d8307dda831ff42016979640bca926910d8b936
Yash Singh <yash9vardhan@gmail.com>
2024-03-22 16:19:48 +0000

    spell error

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
         <div className="flex h-screen w-full justify-center items-center">
             <div className="flex flex-col items-center justify-center mt-[-8%]">
                 <h1 className="text-center text-6xl font-bold bg-clip-text">
-                    Watch Your Faviourt Anime<br />
+                    Watch Your Favourite Anime<br />
                     <div className="animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-6xl font-black">With Friends</div>
                 </h1>
                 <InputBox />
